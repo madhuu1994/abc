230b501441c3a88db5cc96f08e5d3c2b60c2a4a6
madhuu1994 <sharma.madhu821994@gmail.com>
2016-11-19 05:39:25 +1300

    Add files via upload

---
+++ AssignmentQuestion.java
@@ -0,0 +1,16 @@
+class AssignmentQuestion
+{
+	public static void main(String args[])
+	{
+		/*short s1=1;
+		short s2=s1+1;
+		System.out.println(s2);*/
+		byte b1=1;
+		byte b2=(byte)(b1+1);
+		System.out.println(b2);
+		/**
+		Ravina Topiwala
+		*/
+		
+	}
+}
